ebc09a6f58b8b4e9bb0a0c09d77cc5c419cdc865
Fabian-Fynn <fhoffmann.mmt-m2016@fh-salzburg.ac.at>
2017-04-10 18:31:44 +0200

    :sparkles: Feat: Basic functionality

--- 02-chess/src/app.js
+++ 02-chess/src/app.js
@@ -1,2 +1,85 @@
-import config from './config'
+import $ from 'jquery';
+import config from './config';
+import ChessBoard from 'chessboardjs';
+import Chess from 'chess.js';
+window.$ = $;
 
+let board,
+  game = Chess.Chess(),
+  statusEl = $('#status'),
+  fenEl = $('#fen'),
+  pgnEl = $('#pgn');
+window.game = game;
+const onDragStart = function(source, piece, position, orientation) {
+  if (game.game_over() === true ||
+      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+    return false;
+  }
+};
+
+const onDrop = function(source, target) {
+  // see if the move is legal
+  let move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  });
+
+  // illegal move
+  if (move === null) return 'snapback';
+
+  updateStatus();
+};
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+const onSnapEnd = function() {
+  board.position(game.fen());
+};
+
+const updateStatus = function() {
+  let status = '';
+
+  let moveColor = 'White';
+  if (game.turn() === 'b') {
+    moveColor = 'Black';
+  }
+
+  // checkmate?
+  if (game.in_checkmate() === true) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.';
+  }
+
+  // draw?
+  else if (game.in_draw() === true) {
+    status = 'Game over, drawn position';
+  }
+
+  // game still on
+  else {
+    status = moveColor + ' to move';
+
+    // check?
+    if (game.in_check() === true) {
+      status += ', ' + moveColor + ' is in check';
+    }
+  }
+
+  statusEl.html(status);
+  fenEl.html(game.fen());
+  pgnEl.html(game.pgn({ max_width: 5, newline_char: '<br />' }));
+};
+
+const cfg = {
+  draggable: true,
+  pieceTheme: 'images/chesspieces/wikipedia/{piece}.png',
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+};
+
+board = ChessBoard('board', cfg);
+$(window).resize(board.resize);
+updateStatus();
